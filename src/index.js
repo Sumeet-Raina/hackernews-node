17b287518c0136efac8f1ac9c6ffe4e6c3a450a6
Sumeet Raina <sraina@WRLONMBP74.local>
2020-12-17 14:03:19 +0000

    Backend Tutorial

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const path = require('path');
+const { PrismaClient } = require('@prisma/client');
+const { ApolloServer } = require('apollo-server');
+const { PubSub } = require('apollo-server')
+const Subscription = require('./resolvers/Subscription')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User');
+const Link = require('./resolvers/Link');
+const Query = require('./resolvers/Query');
+
+
+const pubsub = new PubSub();
+const prisma = new PrismaClient();
+
+const resolvers = {
+  Query,
+  Mutation,
+  Subscription,
+  User,
+  Link,
+}
+
+
+const server = new ApolloServer({
+  typeDefs: fs.readFileSync(
+    path.join(__dirname, 'schema.graphql'),
+    'utf8'
+  ),
+  resolvers,
+  context: ({ req }) => {
+    return {
+      ...req,
+      prisma,
+      pubsub,
+      userId:
+        req && req.headers.authorization
+          ? getUserId(req)
+          : null
+    };
+  }
+});
+
+server
+  .listen()
+  .then(({ url }) =>
+  console.log(`Server is running on ${url}`)
+  )
